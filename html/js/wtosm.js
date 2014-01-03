f720d65d288d14bfb512a541765fdc6c38c03365
CristianCantoro <kikkocristian@gmail.com>
2014-01-03 11:21:20 +0100

    Visualize Coord template with lat and lon in popup

--- html/js/wtosm.js
+++ html/js/wtosm.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
         $(".missing_template_alert").click(function (event) {
+                event.preventDefault();
+
+                var input = $( this );
+
+                var lat = input.attr( 'data-lat') ;
+                var lon = input.attr( 'data-lon' ) ;
+
                 var msg = "All'articolo in Wikipedia manca il testo per mostrare le coordinate e la mappa OSM (il template Coord).";
                     msg += "\n\nAggiungi in cima alla pagina il seguente codice, completando le coordinate:";
-                    msg += "\n\n{{coord|lat (gradi decimali)|N|long (gradi decimali)|E|display=title}}";
-                    msg += "\n\nPuoi copiare le coordinate da JOSM: scaricando l\'oggetto e cliccando nel riquadro in basso a sinistra.";
+                    msg += "\n\n{{coord|" + lat + "|" + lon + "|display=title}}";
+
                 alert(msg);
         });
 });
