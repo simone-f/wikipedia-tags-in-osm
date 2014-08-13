af4fdefa5fd78c82abefcc2b7b65e85d7879ca03
CristianCantoro <kikkocristian@gmail.com>
2014-08-13 16:39:51 +0200

    Fix bug: convert_pbf_to_o5m is not a method of app

--- osm_downloader.py
+++ osm_downloader.py
@@ -59,7 +59,7 @@ def update_osm_data(app):
         call('mv %s %s' % (app.countryO5M, app.oldCountryO5M), shell=True)
     else:
         print "O5M is missing, I'll try to convert the PBF file..."
-        app.convert_pbf_to_o5m(app)
+        convert_pbf_to_o5m(app)
     call('osmupdate -v -B=%s %s %s' % (app.countryPoly, app.oldCountryO5M, app.countryO5M), shell=True)
     if os.path.isfile(app.countryO5M):
         print "\n- %s has been updated, removing temporary file %s" % (app.countryO5M, app.oldCountryO5M)
